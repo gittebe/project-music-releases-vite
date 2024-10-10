f762f06bd7145733253d6e3102b8146f4472932c
Gitte Beckmann <gittebeckmann@hotmail.com>
2024-10-10 12:55:03 +0200

    - added the icons

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -11,9 +11,13 @@ export const Album = () => {
             {albums.items.map((album) => (
                 <div key={album.id} className="album-card">
                     <CoverImage coverImg={album.images.length > 0 ? album.images[0].url : 'fallback-image-url.jpg'} />
-                    <AlbumName name={album.name} />
-                    <ArtistName artists={album.artists} />
-                    <a href={album.external_urls.spotify} target="_blank" rel="noopener noreferrer">Listen</a>
+
+                    <div className="album-info">
+                        <AlbumName name={album.name} />
+                        <ArtistName artists={album.artists} />
+                        {/* <a href={album.external_urls.spotify} target="_blank" rel="noopener noreferrer">Listen</a> */}
+                    </div>
+
                 </div>
             ))}
         </div>
