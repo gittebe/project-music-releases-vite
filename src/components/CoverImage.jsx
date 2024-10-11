b52558144aea4694f44dc874259a070be0f6835b
Gitte Beckmann <gittebeckmann@hotmail.com>
2024-10-11 19:34:18 +0200

    - added url to play button with onClick; deleted link of coverimage

--- src/components/CoverImage.jsx
+++ src/components/CoverImage.jsx
@@ -3,20 +3,19 @@ import { DotsButton } from "./DotsButton"
 import { HeartButton } from "./HeartButton"
 import { PlayButton } from "./PlayButton"
 
+
 export const CoverImage = ({ coverImg, albumUrl }) => {
     return (
         <div className="cover-image-container">
-            <a href={albumUrl} target="_blank" rel="noopener noreferrer">
-                <div className="cover-image">
-                    <img className="cover-image-img" alt="Album Cover" src={coverImg} />
-
+            <div className="cover-image">
+                <img className="cover-image-img" alt="Album Cover" src={coverImg} />
+                <div className="icon-container">
+                    <HeartButton />
+                    <PlayButton albumUrl={albumUrl} />
+                    <DotsButton />
                 </div>
-            </a>
-            <div className="icon-container">
-                <HeartButton />
-                <PlayButton />
-                <DotsButton />
             </div>
+
         </div>
     )
 }
@@ -25,3 +24,4 @@ CoverImage.propTypes = {
     coverImg: PropTypes.string.isRequired,
     albumUrl: PropTypes.string.isRequired,
 }
+
