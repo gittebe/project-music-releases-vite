3fde8bf1d3ae784d77d018664ca92b766e642ad5
Gitte Beckmann <gittebeckmann@hotmail.com>
2024-10-11 11:19:51 +0200

    - hover effect of images and buttons

--- src/components/HeartButton.jsx
+++ src/components/HeartButton.jsx
@@ -2,7 +2,7 @@ import heartIcon from "../assets/icons/heart.svg"
 
 export const HeartButton = () => {
     return (
-        <span>
+        <span className="heart-button-container">
             <button type="button" aria-label="like" className="heart-button">
                 <img src={heartIcon} className="heart-icon" />
             </button>
--- src/components/PlayButton.jsx
+++ src/components/PlayButton.jsx
@@ -2,7 +2,7 @@ import playIcon from "../assets/icons/play.svg"
 
 export const PlayButton = () => {
     return (
-        <span>
+        <span className="play-button-container">
             <button type="button" aria-label="play" className="play-button">
                 <img src={playIcon} className="play-icon" />
             </button>
