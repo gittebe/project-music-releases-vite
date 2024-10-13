4b5076475602f78d6aefea89b90adf2540d39e5d
Gitte Beckmann <gittebeckmann@hotmail.com>
2024-10-13 11:34:55 +0200

    - added sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import data from "./data.json";
+import stretchgoals from "./stretch-goal.json"
 import { Album } from "./components/Album"
 
 console.log(data);
+console.log(stretchgoals)
 
 export const App = () => {
     return (
--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -3,26 +3,30 @@ import { AlbumName } from "./AlbumName"
 import { ArtistName } from "./ArtistName"
 import { CoverImage } from "./CoverImage"
 import { Header } from "./Header"
+import { Sidebar } from "./Sidebar"
 
 // Album
 export const Album = () => {
     return (
         <>
             <Header />
-            <div className="album-container">
-                {/* iterating over the album and rendering one card for each album */}
-                {albums.items.map((album) => (
-                    <div key={album.id} className="album-card">
-                        <CoverImage coverImg={album.images.length > 0 ? album.images[0].url : 'fallback-image-url.jpg'}
-                            albumUrl={album.external_urls.spotify} />
-                        <div className="album-info">
-                            <AlbumName name={album.name} albumUrl={album.external_urls.spotify} />
-                            <ArtistName artists={album.artists} artistsUrl={album.external_urls.spotify} />
+            <div className="main-container">
+                <div className="album-container">
+                    {albums.items.map((album) => (
+                        <div key={album.id} className="album-card">
+                            <CoverImage
+                                coverImg={album.images.length > 0 ? album.images[0].url : 'fallback-image-url.jpg'}
+                                albumUrl={album.external_urls.spotify}
+                            />
+                            <div className="album-info">
+                                <AlbumName name={album.name} albumUrl={album.external_urls.spotify} />
+                                <ArtistName artists={album.artists} artistsUrl={album.external_urls.spotify} />
+                            </div>
                         </div>
-                    </div>
-                ))
-                }
-            </div >
+                    ))}
+                </div>
+                <Sidebar />
+            </div>
         </>
-    )
-}
+    );
+};
